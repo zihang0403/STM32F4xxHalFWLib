15500338fb3fb0b23b3b1f61c645f2186554d4ee
zihang0403 <zihang0403@qq.com>
2024-01-05 15:38:49 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/TEST.elf.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Core/Startup/startup_stm32f429igtx.s" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Core/Startup/startup_stm32f429igtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F429xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Core/Src/main.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Core/Src/main.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Core/Src/stm32f4xx_hal_msp.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Core/Src/stm32f4xx_it.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Core/Src/syscalls.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Core/Src/syscalls.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Core/Src/sysmem.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Core/Src/sysmem.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Core/Src/system_stm32f4xx.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "D:/ProgramTools/Codefile/keil5file/ARM/STM32F4xxHalFWLib/cmake-build-debug/CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F429xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TEST.elf.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TEST.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/TEST.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/TEST.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/TEST.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/TEST.elf.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/TEST.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "CMakeFiles/TEST.elf.dir/Core/Startup/startup_stm32f429igtx.s.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "CMakeFiles/TEST.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "TEST.elf"
+  "TEST.elf.manifest"
+  "TEST.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/TEST.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
